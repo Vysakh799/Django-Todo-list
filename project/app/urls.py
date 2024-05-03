6b7e7292acde694809807a4c90eadb48c225c7f9
Vysakh799 <vysakh7995@gmail.com>
2024-05-03 14:47:46 +0530

    added update and delete

--- project/app/urls.py
+++ project/app/urls.py
@@ -1,5 +1,8 @@
 from django.urls import path
 from . import views
 urlpatterns = [
-    path('',views.index)    
+    path('',views.index),
+    path('update/<pk>',views.update),
+    path('delete/<pk>',views.delete)
+
 ]
--- project/app/views.py
+++ project/app/views.py
@@ -1,4 +1,4 @@
-from django.shortcuts import render
+from django.shortcuts import render,redirect
 import datetime
 from .models import *
 from django.http import HttpResponse
@@ -23,13 +23,36 @@ def index(request):
 
         
         list=request.POST['list']
-        try:
-            data=todo.objects.get(list=list)
-            print(data)
-            return HttpResponse("list already exist pls update it")
-        except:
-            pass
         data=todo.objects.create(date=formatted_date,list=list)
         data.save()
-    data=todo.objects.all()
-    return render(request,'index.html',{'data':data})
+        return redirect(index)
+        
+    else:
+        data=todo.objects.all()
+        return render(request,'index.html',{'data':data})
+def update(request,pk):
+    data1=todo.objects.get(pk=pk)
+    if request.method=='POST':
+        x=datetime.datetime.now()
+        date=(x.strftime("%x"))
+        date_string = date
+        try:
+        # Split the string at "/"
+            parts = date_string.split("/")
+
+            # Assuming two-digit year, prepend "20"
+            year = "20" + parts[2]
+
+            # Create the formatted date string
+            formatted_date = f"{year}-{parts[0]}-{parts[1]}"
+        except IndexError:
+             print("Invalid date format. Please use MM/DD/YY.")
+        list=request.POST['list']
+        data=todo.objects.filter(pk=pk).update(date=formatted_date,list=list)
+        return redirect(index)
+    else:
+        data=todo.objects.all()
+        return render(request,'index.html',{'data1':data1,'data':data})
+def delete(request,pk):
+    todo.objects.get(pk=pk).delete()
+    return redirect(index)
